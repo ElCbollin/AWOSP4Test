7a4de4d9d5e36a8723c3a74b91a05fd8f821fb5b
0xCbolla <sgaribayguizar@gmail.com>
2026-02-24 17:21:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify, make_response, session
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/preguntas')
+def productos():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_prueba_usr",
+        password="|Au/mc*H2jH3",
+        database="u760464709_prueba_bd"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM Preguntas")
+    myresult = mycursor.fetchall()
+    return make_response(jsonify(myresult))
+
+"""
+@app.post('/insertarPregunta')
+def producto():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_prueba_usr",
+        password="|Au/mc*H2jH3",
+        database="u760464709_prueba_bd"
+    )
+
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO productos (nombre, categoria, precio, existencias) VALUES (%s, %s, %s, %s)"
+    val = (request.form['txtNombre'], request.form['cboCategoria'], request.form['txtPrecio'], request.form['txtExistencias'])
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return "correcto"
+"""
